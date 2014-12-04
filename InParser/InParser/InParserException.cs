9befc51e30b5221a4a8f1e5dc84609544d58b2df
bladelores <bladelores.pls@gmail.com>
2014-12-04 16:37:56 +0300

    testfix7

---
+++ InParser/InParser/InParserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InParser
+{
+    public class InParserException : System.Exception
+    {
+        public InParserException(string message)
+            : base(message)
+        {
+        }
+    }
+
+}
